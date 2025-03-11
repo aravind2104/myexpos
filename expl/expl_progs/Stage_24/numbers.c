1e0732823a2e67bccb20ffee97477da935699b0c
Aravind <aravindanil2101@gmail.com>
2025-03-11 19:10:54 +0530

    Stage 24 assg1

---
+++ expl/expl_progs/Stage_24/numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a = 1;
+    FILE *f = fopen("numbers.dat", "w");
+    while(a<2048){
+        fprintf(f, "%d\n", a);
+        a++;
+    }
+    fclose(f);
+    return 0;
+}
